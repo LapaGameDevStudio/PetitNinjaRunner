c81935b77333bc20cddaa248f5ce8b763a6b5602
Khalid Kejdid <khalidkejdid@gmail.com>
2025-06-22 14:10:25 +0200

    Fix animation for jump run and death

--- player.gd
+++ player.gd
@@ -1,5 +1,4 @@
 extends CharacterBody2D
-
 const GRAVITY = 1000.0
 const JUMP_FORCE = -500.0
 const SPEED = 300.0
@@ -9,26 +8,27 @@ var is_dead = false
 
 func _physics_process(delta):
 	if is_dead:
-		return  # Freeze player control when dead
+		return  # Stop everything when dead
 	velocity.y += GRAVITY * delta
-	
+
 	var direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	velocity.x = direction * SPEED
 
-	# Play correct animation
-	if direction != 0:
-		$AnimatedSprite2D.animation = "RUNNING"
-		$AnimatedSprite2D.flip_h = direction < 0
-	else:
-		$AnimatedSprite2D.animation = "IDLE"
-
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_FORCE
-		$AnimatedSprite2D.animation = "JUMP" # !!!! TO BE FIXED !!!!!
 		$JumpSound.play()
+	if not is_on_floor():
+		$AnimatedSprite2D.animation = "JUMP"
+		$AnimatedSprite2D.play()
+	elif direction != 0:
+		$AnimatedSprite2D.animation = "RUNNING"
+		$AnimatedSprite2D.flip_h = direction < 0
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.animation = "IDLE"
+		$AnimatedSprite2D.play()
 
 	move_and_slide()
-
 	var collision = get_last_slide_collision()
 	if collision:
 		var collider = collision.get_collider()
@@ -37,21 +37,13 @@ func _physics_process(delta):
 
 func bye_bye_amigo():
 	if is_dead:
-		return
+		return  # Avoid double death
 	is_dead = true
-
-	# Stop movement
-	velocity = Vector2.ZERO
-	
-	# Play death animation
+	print("YOU ARE DEAD PITCHOO")
 	$AnimatedSprite2D.animation = "DEATH"
-	$AnimatedSprite2D.frame = 0  # Restart animation
 	$AnimatedSprite2D.play()
+	
+	# Optionnel : attendre un peu avant de signaler la fin
+	await get_tree().create_timer(1).timeout  # Laisse le temps à l'anim de démarrer
 
-	# Play sound
-	$"../GameOver_UI/GameOverSound".play()
-
-	# Delay game over until animation finishes
-	var anim_length = $AnimatedSprite2D.sprite_frames.get_frame_count("DEATH") / $AnimatedSprite2D.sprite_frames.get_animation_speed("DEATH")
-	await get_tree().create_timer(anim_length).timeout
 	emit_signal("game_over")
