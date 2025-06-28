ea5980750207dad5380a2c5e34fa58d6549405f7
Khalid Kejdid <khalidkejdid@gmail.com>
2025-06-28 19:09:04 +0200

    issue23 - fix player animation and add fluidity movement of camera

--- cannon_spawner_t_3.gd
+++ cannon_spawner_t_3.gd
@@ -3,16 +3,15 @@ extends StaticBody2D
 @export var CannonBall: PackedScene
 @export var throw_interval: float = 2.0
 @export var fire_direction: int = -1  # -1 = gauche, 1 = droite
-@export var spawn_offset: Vector2 = Vector2(0, 0)  # relatif au canon
+@export var spawn_offset: Vector2 = Vector2(-60, -5)  # relatif au canon
 @export var cannon_texture: Texture2D
 @export var ball_speed: float = 400.0  # 🆕 Vitesse personnalisée
 
-
 func _ready():
-	$ThrowTimer.wait_time = throw_interval
-	if not $ThrowTimer.timeout.is_connected(_on_throw_timer_timeout):
-		$ThrowTimer.timeout.connect(_on_throw_timer_timeout)
-	$ThrowTimer.start()
+	$Timer.wait_time = throw_interval
+	if not $Timer.timeout.is_connected(_on_throw_timer_timeout):
+		$Timer.timeout.connect(_on_throw_timer_timeout)
+	$Timer.start()
 	if cannon_texture:
 		$Sprite2D.texture = cannon_texture
 		
@@ -32,4 +31,4 @@ func shoot():
 	ball.position = spawn_offset  # position locale
 	add_child(ball)  # la balle devient enfant du canon
 	ball.launch_rocket(fire_direction, ball_speed)
-	print("💣 CannonBall fired at:", ball.global_position, "→ Direction:", fire_direction)
+	print("💣 CannonBall fired at:", ball.global_position, "→ Direction:", fire_direction, "speed:", ball_speed)
